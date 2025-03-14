b19f7edc64214fc9ac21b347109a0af673c1298f
AdCatxt <adcatzt@gmail.com>
2025-03-14 08:22:43 -0600

    Auth for routes

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -1,9 +1,13 @@
 import { applyDecorators, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "../guards/auth.guard";
+import { ROLES } from "../constants/roles.constants";
 import { Roles } from "./roles.decorator";
 import { RolesGuard } from "../guards/roles.guard"; 
 
-export const Auth = (...roles: string[]) => applyDecorators(
-    Roles(roles),
-    UseGuards(AuthGuard, RolesGuard),
-)
+export const Auth = (...roles: ROLES[]) => {
+    roles.push(ROLES.ADMIN);
+    return applyDecorators(
+        Roles(roles),
+        UseGuards(AuthGuard, RolesGuard),
+    )
+}
--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -6,28 +6,34 @@ import { UpdateEmployeeDto } from './dto/update-employee.dto';
 import { v4 as uuid } from 'uuid';
 import { ParseUUIDPipe } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constants';
 
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
+  @Auth(ROLES.MANAGER)
   @Post()
   create(@Body() createEmployeeDto: CreateEmployeeDto) {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @Auth(ROLES.MANAGER, ROLES.EMPLOYEE)
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   uploadPhoto(@UploadedFile() file: Express.Multer.File) {
     return 'OK'
   }
 
+  @Auth(ROLES.MANAGER)
   @Get()
   findAll() {
     return this.employeesService.findAll();
   }
 
+  @Auth(ROLES.MANAGER)
   @Get('/:id')
   findOne(
       @Param('id', new ParseUUIDPipe({ version: '4' }))
@@ -36,6 +42,7 @@ export class EmployeesController {
       return this.employeesService.findOne(id);
   }
   
+  @Auth(ROLES.EMPLOYEE)
   @Patch('/:id')
   update(
       @Param('id', new ParseUUIDPipe({ version: '4' }))
@@ -45,6 +52,7 @@ export class EmployeesController {
       return this.employeesService.update(id, updateEmployeeDto);
   }
   
+  @Auth(ROLES.MANAGER)
   @Delete('/:id')
   remove(
       @Param('id', new ParseUUIDPipe({ version: '4' }))
