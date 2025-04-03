2c8731cd18ec5d5a3410bc362ab23bc022c293bb
AdCatxt <adcatzt@gmail.com>
2025-04-03 17:32:12 -0600

    Fix: return location on create

--- src/auth/constants/jwt.constants.ts
+++ src/auth/constants/jwt.constants.ts
@@ -1,3 +1,3 @@
-export const JWT_KEY = 'EUIDSJBVUISN#$#$fs#$'
-export const EXPIRES_IN = '7d'
-export const TOKEN_NAME = 'auth_for_ocxo'
+export const JWT_KEY = 'EUIDSJBVUISN#$#$fs#$';
+export const EXPIRES_IN = '7d';
+export const TOKEN_NAME = 'auth_for_ocxo';
